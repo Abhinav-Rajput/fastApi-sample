a8cb5af01d6a3d7d60c03f47022800173670f0ac
Abhinav-Rajput <abhinavrajput2011@gmail.com>
2022-09-04 10:09:16 -0400

    basic routing & query parameters

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
+from typing import Optional
 from fastapi import FastAPI
 
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data':{'name':'Abhinav'}}
+@app.get('/blog')
+def index(limit=10,published:bool=True,sort:Optional[str]=None):
+    # return published
+    if published:
+        return {'data':f'{limit} published blog  list from db'}
+    return {'data':f'{limit}  all blog  list from db'}
 
 
-@app.get('/about')
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data' : 'all unpublished blog'}
+
+@app.get('/blog/{id}')
+def show(id:int):
+    return {'data':id}
+
+
+@app.get('/blog/{id}/comments')
+def show(id:int):
+    return {'data': {'1','2'}}
+
 def about():
     return {'data':{'page':'about1'}}
 
